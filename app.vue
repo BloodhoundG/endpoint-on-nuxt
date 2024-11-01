d91472952a5bd9d4df75d78c51d788c4c0faa35b
Alexander Nefedov <sasha.nef@yandex.ru>
2024-11-01 17:19:35 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage />
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/hello");
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/hello");
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  font-size: 40px;
+  margin: 20px 0 0;
+}
+</style>
